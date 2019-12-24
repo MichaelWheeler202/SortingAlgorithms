fb805daab7de2b2dedfc10e71296b7bf797ef337
Mikew <mikewheeler202@gmail.com>
2019-12-24 12:49:09 -0500

    Added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SortingAlgorithms',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Mikew',
+    author_email='',
+    description=''
+)
